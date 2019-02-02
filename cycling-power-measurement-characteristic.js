a34b0d22d1375aa014f34d665a24a4a435e85af7
potti3000 <47273193+potti3000@users.noreply.github.com>
2019-02-02 15:21:18 +0100

    Update cycling-power-measurement-characteristic.js

--- cycling-power-measurement-characteristic.js
+++ cycling-power-measurement-characteristic.js
@@ -73,28 +73,28 @@ CyclingPowerMeasurementCharacteristic.prototype.notify = function(event) {
   debug("power: " + watts);
   buffer.writeInt16LE(watts, 2);
   
-  if ('speed_cm_s' in event) {
-    var delta = (now - last_speed);
-    var speed = event.speed_cm_s * 10; // mm/s
-    var rev = Math.floor (speed * delta / wheel_circ / 1000);
-    debug("\n\rspeed: " + speed + " rev: " + rev);
-    debug ('now/last: ' + now +  ' / ' + last_speed);
-    if (rev != 0) {
-      last_rev += rev;
-      last_speed += Math.floor ((wheel_circ * rev) * 1000 / speed);
-      debug("new_speed/delta: " + last_speed + " / " + delta);
-      buffer.writeUInt32LE(last_rev, pos);
-      buffer.writeUInt16LE((last_speed * 2048 / 1000)% 65536, pos+4);
-      pos += 6;
-      flags |= 0x010;
-    }
+  if ('wheel_rev' in event && 'wheel_time' in event) {
+   
+    var rev = event.wheel_rev;
+   
+    buffer.writeUInt32LE(rev, pos);
+
+    //time definition
+    var now_1024 = Math.floor(event.wheel_time*2048/1000);
+    var event_time = now_1024 % 65536; // rolls over every 64 seconds
+
+    buffer.writeUInt16LE(event_time, pos+4);
+    
+    pos += 6;
+    flags |= 0x010;
+
   }
 
-  if ('rev_count' in event) {
+  if ('rev_count' in event && 'rev_time' in event) {
     debug("rev_count: " + event.rev_count);
     buffer.writeUInt16LE(event.rev_count, pos);
     
-    var now_1024 = Math.floor(now*1024/1000);
+    var now_1024 = Math.floor(event.rev_time*1024/1000);
     var event_time = now_1024 % 65536; // rolls over every 64 seconds
     debug("event time: " + event_time);
     buffer.writeUInt16LE(event_time, pos+2);
